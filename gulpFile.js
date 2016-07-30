264270603bec43fa49679f5cacf2b788422a5f22
Venkata Krishna <venkatakrishna.1991@gmail.com>
2016-07-30 18:35:13 -0400

    Added compass

--- gulpFile.js
+++ gulpFile.js
@@ -2,9 +2,12 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
+
 var coffeeSources = ['Compounents/coffee/tagline.coffee'];
 var jsSource = ['Compounents/scripts/*.js'];
+var sassSources = ['Compounents/sass/style.scss'];
 gulp.task('log', function () {
 	gutil.log('Welcome gulp');
 });
@@ -19,7 +22,17 @@ gulp.task('coffee', function () {
 gulp.task('js', function () {
 	gulp.src(jsSource)
 		.pipe(concat('scrip.js'))
-	    .pipe(browserify())
+		.pipe(browserify())
 		.pipe(gulp.dest('Builds/Development/JS'));
 
+});
+gulp.task('compass', function () {
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'Compounents/sass',
+			image: 'Builds/Development/images',
+			style: 'expanded'
+		}))
+		.on('error', gutil.log)
+		.pipe(gulp.dest('Builds/Development/CSS'));
 });
