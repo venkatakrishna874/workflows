ea85594a0904e60777378198be653019fe9e2c8e
Venkata Krishna <venkatakrishna.1991@gmail.com>
2016-07-30 17:58:24 -0400

    concatenated JS files

--- gulpFile.js
+++ gulpFile.js
@@ -1,14 +1,23 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee');
-var coffeeSource = ['Compounents/coffee/*.coffee'];
-gulp.task('log', function() {
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat');
+var coffeeSources = ['Compounents/coffee/tagline.coffee'];
+var jsSource = ['Compounents/scripts/*.js'];
+gulp.task('log', function () {
 	gutil.log('Welcome gulp');
 });
-gulp.task('coffee', function() {
-	gulp.src(coffeeSource)
-	.pipe(coffee({bare:true})
-		 .on('error', gutil.log)
-		  .pipe(gulp.dest('Compounents/scripts'))
-		 );
+gulp.task('coffee', function () {
+	gulp.src(coffeeSources)
+		.pipe(coffee({
+				bare: true
+			})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'))
+});
+gulp.task('js', function () {
+	gulp.src(jsSource)
+		.pipe(concat('scrip.js'))
+		.pipe(gulp.dest('Builds/Development/JS'));
+
 });
