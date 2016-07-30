b43f55c47b8b1c2a5ea73a570909136354a908ea
Venkata Krishna <venkatakrishna.1991@gmail.com>
2016-07-30 17:26:32 -0400

    Added coffee task to gulp file

--- gulpFile.js
+++ gulpFile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
+var coffeeSource = ['Compounents/coffee/*.coffee'];
 gulp.task('log', function() {
 	gutil.log('Welcome gulp');
+});
+gulp.task('coffee', function() {
+	gulp.src(coffeeSource)
+	.pipe(coffee({bare:true})
+		 .on('error', gutil.log)
+		  .pipe(gulp.dest('Compounents/scripts'))
+		 );
 });
