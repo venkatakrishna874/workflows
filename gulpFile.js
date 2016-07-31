72318d847628739c159c3db42568d056279b1ec9
Venkata Krishna <venkatakrishna.1991@gmail.com>
2016-07-31 00:05:57 -0400

    Added image min

--- gulpFile.js
+++ gulpFile.js
@@ -7,6 +7,9 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	gulpif = require('gulp-if'),
 	minifyHTML = require('gulp-minify-html'),
+	imagemin = require('gulp-imagemin'),
+	jsonmin = require('gulp-jsonminify'),
+	pngcrush = require('imagemin-pngcrush'),
 	concat = require('gulp-concat');
 
 var coffeeSources,
@@ -46,7 +49,7 @@ gulp.task('js', function () {
 	gulp.src(jsSource)
 		.pipe(concat('script.js'))
 		.pipe(browserify())
-	.pipe(gulpif(env === 'production', uglify()))
+		.pipe(gulpif(env === 'production', uglify()))
 		.pipe(gulp.dest(outputDir + 'JS'))
 		.pipe(connect.reload());
 
@@ -64,8 +67,9 @@ gulp.task('compass', function () {
 });
 gulp.task('watch', function () {
 	gulp.watch('Compounents/**/*', ['coffee', 'js', 'compass']);
-	gulp.watch(htmlSources, ['html']);
-	gulp.watch(jsonSource, ['json']);
+	gulp.watch('Builds/Development/*.html', ['html']);
+	gulp.watch('Builds/Development/JS/*.json', ['json']);
+	gulp.watch('Builds/Development/images/**/*.*');
 });
 gulp.task('connect', function () {
 	connect.server({
@@ -76,13 +80,25 @@ gulp.task('connect', function () {
 htmlSources = [outputDir + '*.html']
 gulp.task('html', function () {
 	gulp.src('Builds/Development/*.html')
-	.pipe(gulpif(env === 'production', minifyHTML()))
-	.pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+		.pipe(gulpif(env === 'production', minifyHTML()))
+		.pipe(gulpif(env === 'production', gulp.dest(outputDir)))
 		.pipe(connect.reload());
 });
 jsonSource = [outputDir + 'JS/*.json'];
 gulp.task('json', function () {
-	gulp.src(jsonSource)
+	gulp.src('Builds/Development/JS/*.json')
+	.pipe(gulpif(env === 'production', jsonmin()))
+	.pipe(gulpif(env === 'production', gulp.dest( 'Builds/production/JS/')))
 		.pipe(connect.reload());
 });
-gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('imagemin', function() {
+	gulp.src('Builds/Development/images/**/*.*')
+	.pipe(gulpif(env === 'production', imagemin({
+		progressive:true,
+		svgPlugins:[{removeViewbox:false}],
+		use:[pngcrush]
+	})))
+	.pipe(gulpif(env === 'production', gulp.dest('Builds/production/images/')))
+	.pipe(connect.reload())
+})
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass','imagemin' ,'connect', 'watch']);
